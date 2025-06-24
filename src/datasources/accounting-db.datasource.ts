4310a014c1f9bc715bba1af6b53b97222f8c19d2
Rober20 <rrodriguez@kenmeina.com>
2025-06-24 14:16:36 -0600

    fIX: VARIABLES DE ENTORNO

--- src/datasources/accounting-db.datasource.ts
+++ src/datasources/accounting-db.datasource.ts
@@ -1,15 +1,26 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
+// const config = {
+//   name: 'accountingDB',
+//   connector: 'mysql',
+//   url: '',
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: 'root',
+//   database: 'Accounting',
+// };
+
 const config = {
   name: 'accountingDB',
   connector: 'mysql',
-  url: '',
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'Accounting',
+  url: process.env.MYSQL_URL,
+  host: process.env.MYSQL_HOST,
+  port: Number(process.env.MYSQL_PORT),
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 };
 
 // Observe application's life cycle to disconnect the datasource when
