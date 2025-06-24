4788c99a000a94fd15a4f94744f0fcceffd86b2e
Rober20 <rrodriguez@kenmeina.com>
2025-06-24 14:45:30 -0600

    Fix: valiable de entorno

--- src/datasources/accounting-db.datasource.ts
+++ src/datasources/accounting-db.datasource.ts
@@ -16,11 +16,10 @@ const config = {
   name: 'accountingDB',
   connector: 'mysql',
   url: process.env.MYSQL_URL,
-  host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
+  // port: 3306,
+  // user: 'root',
+  // password: 'pDoKhBKuTDiGRsfaGpniqVTFuIohOrQQ',
+  // database: 'accounting',
 };
 
 // Observe application's life cycle to disconnect the datasource when
