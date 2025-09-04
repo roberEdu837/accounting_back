6f016483e55d5c04f52143665ccc1434151f2826
Rober20 <rrodriguez@kenmeina.com>
2025-09-04 14:55:08 -0600

    first commit

--- src/datasources/accounting-db.datasource.ts
+++ src/datasources/accounting-db.datasource.ts
@@ -1,25 +1,26 @@
 import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
 import {juggler} from '@loopback/repository';
 
-// const config = {
-//   name: 'accountingDB',
-//   connector: 'mysql',
-//   host: 'shuttle.proxy.rlwy.net',
-//   port: 3306,
-//   user: 'root',
-//   password: 'QWKDxFPYnEgWEiqaTfwjZZxiQMTHeAcX',
-//   database: 'accountingDB',
-// };
-
 const config = {
   name: 'accountingDB',
   connector: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'accounting',
+  url: 'mysql://root:FvJmohHUUlouvxpADqCEktXsCshOuFVE@yamabiko.proxy.rlwy.net:34086/accountingDB',
+  // host: 'shuttle.proxy.rlwy.net',
+  // port: 3306,
+  // user: 'root',
+  // password: 'QWKDxFPYnEgWEiqaTfwjZZxiQMTHeAcX',
+  // database: 'accountingDB',
 };
+
+// const config = {
+//   name: 'accountingDB',
+//   connector: 'mysql',
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: 'root',
+//   database: 'accounting',
+// };
 
 // Observe application's life cycle to disconnect the datasource when
 // application is stopped. This allows the application to be shut down
