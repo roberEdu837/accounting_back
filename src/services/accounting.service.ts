056de1012a06c7d4e8186203a77fac5b9f6f483c
Rober20 <rrodriguez@kenmeina.com>
2025-09-08 19:21:27 -0600

    Fix: Cambio en sercio de contabilidad

--- src/services/accounting.service.ts
+++ src/services/accounting.service.ts
@@ -21,106 +21,20 @@ export class AccountingService {
   ): Promise<void> {
     const today = new Date();
 
-    //let currentMonth = today.getMonth() + 1;
     let currentMonth = month ? month : today.getMonth() + 1;
-    let year = today.getFullYear();
-    let nextMonth = 0;
+    let year = 2026;
     let currentYear = year;
-    const originalMonth = currentMonth;
 
     if (periodicity === 'BIMESTRAL') {
-      switch (currentMonth) {
-        case 12: // Diciembre
-        case 1: // Enero
-          //case 2: // Febrero
-
-          currentMonth = 11;
-          // year = year - 1;
-          break;
-        case 2: // Febrero
-        case 3: // Marzo
-          //case 4: // Abril
-          currentMonth = 1;
-          break;
-        case 4: // Abril
-        case 5: // Mayo
-          //case 6: // Junio
-          currentMonth = 3;
-          break;
-        case 6: // Junio
-        case 7: // Julio
-          //case 8: // Agosto
-          currentMonth = 5;
-          break;
-        case 8: // Agosto
-        case 9: // Septiembre
-          ///case 10: // Octubre
-          currentMonth = 7;
-          break;
-        case 10: // Octubre
-        case 11: // Noviembre
-          currentMonth = 9;
-          break;
-
-        default:
-          throw new Error('Mes inválido');
-      }
+      const Bimonthly = this.getBimonthlyMonth(currentMonth, periodicity, year);
+      currentMonth = Bimonthly?.month ? Bimonthly.month : currentMonth;
+      year = Bimonthly?.year ? Bimonthly.year : year;
     }
 
     if (periodicity === 'MENSUAL') {
-      switch (currentMonth) {
-        case 1: // Enero
-          currentMonth = 12;
-          nextMonth = 1;
-          year = year - 1;
-          break;
-        case 2: // Febrero
-          currentMonth = 1;
-          nextMonth = 2;
-          break;
-        case 3: // Marzo
-          currentMonth = 2;
-          nextMonth = 3;
-          break;
-        case 4: // Abril
-          currentMonth = 3;
-          nextMonth = 4;
-          break;
-        case 5: // Mayo
-          currentMonth = 4;
-          nextMonth = 5;
-          break;
-        case 6: // Junio
-          currentMonth = 5;
-          nextMonth = 6;
-          break;
-        case 7: // Julio
-          currentMonth = 6;
-          nextMonth = 7;
-          break;
-        case 8: // Agosto
-          currentMonth = 7;
-          nextMonth = 8;
-          break;
-        case 9: // Septiembre
-          currentMonth = 8;
-          nextMonth = 9;
-          break;
-        case 10: // Octubre
-          currentMonth = 9;
-          nextMonth = 10;
-          break;
-        case 11: // Noviembre
-          currentMonth = 10;
-          nextMonth = 11;
-          break;
-        case 12: // Diciembre
-          currentMonth = 11;
-          nextMonth = 12;
-          break;
-        default:
-          throw new Error('Mes inválido');
-      }
+      const Monthly = this.getMonthlyMonth(currentMonth, periodicity, year);
+      currentMonth = Monthly?.prevMonth ? Monthly.prevMonth : currentMonth;
+      year = Monthly?.year ? Monthly.year : year;
     }
 
     const accounting: any = {
@@ -134,20 +48,17 @@ export class AccountingService {
       rfcTaxPaymentDate: await this.getPaymentDate(
         rfc,
         periodicity,
-        month ? month : originalMonth,
+        month ? month : 0,
         year,
       ),
       monthlyPaymentCompleted: false,
     };
-    console.log(accounting, 'accounting por crear');
-    console.log(year, 'año');
-    console.log(currentMonth, 'mes2');
 
     const existingAccounting = await this.monthlyAccountingRepository.findOne({
       where: {
         customerId: customerId,
         month: currentMonth,
-        year: currentYear,
+        year: month === 1 ? currentYear - 1 : currentYear,
       },
     });
 
@@ -221,6 +132,70 @@ export class AccountingService {
         return 5;
       default:
         return 0; // por si no es un dígito válido
+    }
+  }
+  getMonthlyMonth(currentMonth: number, periodicity: string, year: number) {
+    if (!currentMonth || periodicity != 'MENSUAL') return;
+    switch (currentMonth) {
+      case 1: // Enero
+        return {prevMonth: 12, currMonth: 1, year: year - 1};
+      case 2: // Febrero
+        return {prevMonth: 1, currMonth: 2, year};
+      case 3: // Marzo
+        return {prevMonth: 2, currMonth: 3, year};
+      case 4: // Abril
+        return {prevMonth: 3, currMonth: 4, year};
+      case 5: // Mayo
+        return {prevMonth: 4, currMonth: 5, year};
+      case 6: // Junio
+        return {prevMonth: 5, currMonth: 6, year};
+      case 7: // Julio
+        return {prevMonth: 6, currMonth: 7, year};
+      case 8: // Agosto
+        return {prevMonth: 7, currMonth: 8, year};
+      case 9: // Septiembre
+        return {prevMonth: 8, currMonth: 9, year};
+      case 10: // Octubre
+        return {prevMonth: 9, currMonth: 10, year};
+      case 11: // Noviembre
+        return {prevMonth: 10, currMonth: 11, year};
+      case 12: // Diciembre
+        return {prevMonth: 11, currMonth: 12, year};
+      default:
+        throw new Error('Mes inválido');
+    }
+  }
+
+  getBimonthlyMonth(month: number, periodicity: string, year: number) {
+    if (!month || periodicity !== 'BIMESTRAL') return;
+
+    switch (month) {
+      case 12: // Diciembre
+      case 1: // Enero
+        return {month: 11, year: month === 12 ? year : year - 1}; // Noviembre
+
+      case 2: // Febrero
+      case 3: // Marzo
+        return {month: 1, year}; // Enero
+
+      case 4: // Abril
+      case 5: // Mayo
+        return {month: 3, year}; // Marzo
+
+      case 6: // Junio
+      case 7: // Julio
+        return {month: 5, year}; // Mayo
+
+      case 8: // Agosto
+      case 9: // Septiembre
+        return {month: 7, year}; // Julio
+
+      case 10: // Octubre
+      case 11: // Noviembre
+        return {month: 9, year}; // Septiembre
+
+      default:
+        throw new Error('Mes inválido');
     }
   }
 }
