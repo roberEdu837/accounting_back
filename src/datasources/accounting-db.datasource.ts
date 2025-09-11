46e2a81496bef5229aedfa73638f57aed97ee869
Rober20 <rrodriguez@kenmeina.com>
2025-09-11 10:06:32 -0600

    Fix: Cambio a date string

--- src/datasources/accounting-db.datasource.ts
+++ src/datasources/accounting-db.datasource.ts
@@ -5,11 +5,6 @@ const config = {
   name: 'accountingDB',
   connector: 'mysql',
   url: 'mysql://root:SqiilfJFKAUjVrqvBxLTtUSknFJuNDSq@crossover.proxy.rlwy.net:36461/accountingDB',
-  // host: 'crossover.proxy.rlwy.net',
-  // port: 36461,
-  // user: 'root',
-  // password: 'SqiilfJFKAUjVrqvBxLTtUSknFJuNDSq',
-  // database: 'accountingDB',
   dateStrings: true,
 };
 
