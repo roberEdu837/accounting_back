7aa82b68dff45b6a5f4a9471c7a475d7e569578a
Rober20 <rrodriguez@kenmeina.com>
2025-09-11 20:02:51 -0600

    Fix. Corn

--- src/cron-jobs/email.cronjob.ts
+++ src/cron-jobs/email.cronjob.ts
@@ -1,34 +1,34 @@
-// import {service} from '@loopback/core';
-// import {CronJob, cronJob} from '@loopback/cron';
-// import {CustomerService} from '../services/customer.service';
-// import {EmailService} from '../services/email.service';
+import {service} from '@loopback/core';
+import {CronJob, cronJob} from '@loopback/cron';
+import {CustomerService} from '../services/customer.service';
+import {EmailService} from '../services/email.service';
 
-// @cronJob()
-// export class MyCronJob extends CronJob {
-//   constructor(
-//     @service() private customerService: CustomerService,
-//     @service() private emailService: EmailService,
-//   ) {
-//     super({
-//       name: 'EmailJob',
-//       onTick: async () => {
-//         const users = await this.customerService.getCustomerExpereFIEL();
-//         users?.map(async customer => {
-//           await this.emailService.sendEmail(
-//             'Hanscontador@hotmail.com',
-//             'Recordatorio de vencimiento de FIEL',
-//             `Estimado Hans, le recordamos que la FIEL del cliente "${customer.socialReason}" esta por vencer.`,
-//           );
-//           if (customer) {
-//             await this.customerService.updateNotificationStatus(
-//               customer.id,
-//               true,
-//             );
-//           }
-//         });
-//       },
-//       cronTime: '0 2 * * 0',
-//       start: true,
-//     });
-//   }
-// }
+@cronJob()
+export class MyCronJob extends CronJob {
+  constructor(
+    @service() private customerService: CustomerService,
+    @service() private emailService: EmailService,
+  ) {
+    super({
+      name: 'EmailJob',
+      onTick: async () => {
+        const users = await this.customerService.getCustomerExpereFIEL();
+        users?.map(async customer => {
+          await this.emailService.sendEmail(
+            'Hanscontador@hotmail.com',
+            'Recordatorio de vencimiento de FIEL',
+            `Estimado Hans, le recordamos que la FIEL del cliente "${customer.socialReason}" esta por vencer.`,
+          );
+          if (customer) {
+            await this.customerService.updateNotificationStatus(
+              customer.id,
+              true,
+            );
+          }
+        });
+      },
+      cronTime: '0 2 * * *',
+      start: true,
+    });
+  }
+}
